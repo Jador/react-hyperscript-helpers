a28e01416660f705b471e4056144be8c74806c54
Tyler Graham <tyler.graham.prog@gmail.com>
2018-06-27 15:27:35 -0700

    add umd build; remove ancient typescript bindings, will improve in future version

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import { TAG_NAMES } from './tag-names.js';
 
-const isArray    = x => Array.isArray(x);
-const isString   = x => typeof x === 'string' && x.length > 0;
+const isArray = x => Array.isArray(x);
+const isString = x => typeof x === 'string' && x.length > 0;
 const isSelector = x => isString(x) && (startsWith(x, '.') || startsWith(x, '#'));
 const isChildren = x => /string|number|boolean/.test(typeof x) || isArray(x);
 const startsWith = (string, start) => string.indexOf(start) === 0;
-const split      = (string, separator) => string.split(separator);
-const subString  = (string, start, end) => string.substring(start, end);
+const split = (string, separator) => string.split(separator);
+const subString = (string, start, end) => string.substring(start, end);
 
 const parseSelector = selector => {
   const classIdSplit = /([\.#]?[a-zA-Z0-9\u007F-\uFFFF_:-]+)/;
-  const parts        = split(selector, classIdSplit);
+  const parts = split(selector, classIdSplit);
 
   return parts.reduce((acc, part) => {
 
@@ -32,7 +31,7 @@ const createElement = (nameOrType, properties = {}, children = []) => {
   }
 
   const { isRendered, ...props } = properties;
-  const args = [ nameOrType, props ];
+  const args = [nameOrType, props];
 
   if (!isArray(children)) {
     args.push(children);
@@ -79,6 +78,21 @@ export const hh = nameOrType => (first, second, third) => {
 };
 
 const h = (nameOrType, ...rest) => hh(nameOrType)(...rest);
+
+const TAG_NAMES = [
+  'a', 'abbr', 'address', 'area', 'article', 'aside', 'audio', 'b', 'base', 'bdi', 'bdo',
+  'big', 'blockquote', 'body', 'br', 'button', 'canvas', 'caption', 'cite', 'code', 'col',
+  'colgroup', 'data', 'datalist', 'dd', 'del', 'details', 'dfn', 'dialog', 'div', 'dl', 'dt',
+  'em', 'embed', 'fieldset', 'figcaption', 'figure', 'footer', 'form', 'h1', 'h2', 'h3', 'h4',
+  'h5', 'h6', 'head', 'header', 'hgroup', 'hr', 'html', 'i', 'iframe', 'img', 'input', 'ins',
+  'kbd', 'keygen', 'label', 'legend', 'li', 'link', 'main', 'map', 'mark', 'menu', 'menuitem',
+  'meta', 'meter', 'nav', 'noscript', 'object', 'ol', 'optgroup', 'option', 'output', 'p', 'param',
+  'picture', 'pre', 'progress', 'q', 'rp', 'rt', 'ruby', 's', 'samp', 'script', 'section', 'select',
+  'small', 'source', 'span', 'strong', 'style', 'sub', 'summary', 'sup', 'table', 'tbody', 'td',
+  'textarea', 'tfoot', 'th', 'thead', 'time', 'title', 'tr', 'track', 'u', 'ul', 'video',
+  'wbr', 'circle', 'clipPath', 'defs', 'ellipse', 'g', 'image', 'line', 'linearGradient', 'mask',
+  'path', 'pattern', 'polygon', 'polyline', 'radialGradient', 'rect', 'stop', 'svg', 'text', 'tspan'
+];
 
 module.exports = TAG_NAMES.reduce((exported, type) => {
   exported[type] = hh(type);
